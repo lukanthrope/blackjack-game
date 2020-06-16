b234249d157f0908ed69069e2a36742083a8b01c
lukanthrope <bydlocoder228@gmail.com>
2020-06-16 13:36:43 +0300

    some styling stuff

--- blackjack-game/Dashboard.Designer.cs
+++ blackjack-game/Dashboard.Designer.cs
@@ -30,13 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.money = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.closeButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.playAgain = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.money = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -45,7 +45,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.name,
@@ -57,33 +57,17 @@
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(342, 284);
+            this.dataGridView1.Size = new System.Drawing.Size(338, 284);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // name
-            // 
-            this.name.HeaderText = "name";
-            this.name.MinimumWidth = 6;
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            this.name.Width = 125;
-            // 
-            // money
-            // 
-            this.money.HeaderText = "money";
-            this.money.MinimumWidth = 6;
-            this.money.Name = "money";
-            this.money.ReadOnly = true;
-            this.money.Width = 125;
             // 
             // closeButton
             // 
-            this.closeButton.BackColor = System.Drawing.Color.LightCoral;
+            this.closeButton.BackColor = System.Drawing.Color.SlateBlue;
             this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.closeButton.ForeColor = System.Drawing.SystemColors.Control;
-            this.closeButton.Location = new System.Drawing.Point(392, 545);
+            this.closeButton.Location = new System.Drawing.Point(532, 510);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(152, 52);
+            this.closeButton.Size = new System.Drawing.Size(152, 42);
             this.closeButton.TabIndex = 1;
             this.closeButton.Text = "Вийти";
             this.closeButton.UseVisualStyleBackColor = false;
@@ -92,10 +76,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(374, 72);
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(345, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 41);
+            this.label1.Size = new System.Drawing.Size(244, 53);
             this.label1.TabIndex = 2;
             this.label1.Text = "Топ гравців";
             // 
@@ -104,7 +88,7 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.linkLabel1.Location = new System.Drawing.Point(727, 476);
+            this.linkLabel1.Location = new System.Drawing.Point(218, 555);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(81, 20);
             this.linkLabel1.TabIndex = 3;
@@ -115,7 +99,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(731, 412);
+            this.pictureBox1.Location = new System.Drawing.Point(222, 491);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(68, 61);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -127,13 +111,29 @@
             this.playAgain.BackColor = System.Drawing.Color.SlateBlue;
             this.playAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.playAgain.ForeColor = System.Drawing.SystemColors.Control;
-            this.playAgain.Location = new System.Drawing.Point(392, 476);
+            this.playAgain.Location = new System.Drawing.Point(341, 510);
             this.playAgain.Name = "playAgain";
             this.playAgain.Size = new System.Drawing.Size(152, 42);
             this.playAgain.TabIndex = 5;
             this.playAgain.Text = "Зіграти ще";
             this.playAgain.UseVisualStyleBackColor = false;
             this.playAgain.Click += new System.EventHandler(this.PlayAgain_Click);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Ім\'я";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 125;
+            // 
+            // money
+            // 
+            this.money.HeaderText = "Капітал";
+            this.money.MinimumWidth = 6;
+            this.money.Name = "money";
+            this.money.ReadOnly = true;
+            this.money.Width = 125;
             // 
             // Dashboard
             // 
@@ -159,12 +159,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn money;
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button playAgain;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn money;
     }
 }
